deb10255159836704cfaf9c8f040661eae66f1df
redp4danila <142677300+redp4danila@users.noreply.github.com>
2023-09-30 23:56:22 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94766801421', 'me', true],
+  ['94766801421', '🇱🇰̽ ᴛ̲ͫ ᴇͬ ̲ᴅ ̲ɪ ᭄⃝̲🎩ˣ', true], 
   [''] 
 ] //Number of owners
 
@@ -47,7 +47,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'ᴛʜᴇ 🇱🇰̽ ᴛ̲ͫ ᴇͬ ̲ᴅ ̲ɪ ᭄⃝̲🎩ˣ ʙᴏᴛ'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
